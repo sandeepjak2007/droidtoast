5dcebd468bbc0b8d72ae647a6f2d3f8697e7704b
Sandeep K <>
2023-08-30 12:31:46 +0530

    Custom Alert Drawables, layout and required values

--- app/src/main/java/code/sandeep/droidtoast/MainActivity.kt
+++ app/src/main/java/code/sandeep/droidtoast/MainActivity.kt
@@ -1,13 +1,19 @@
 package code.sandeep.droidtoast
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DroidToast.showToast(this,"Simple toast from sample library")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        DroidToast.showToast(this, "Simple Failure Message from sample library", status = DroidToast.FAILURE)
+        DroidToast.showToast(this, "Simple Success Message from sample library", status = DroidToast.SUCCESS)
+        DroidToast.showToast(this, "Simple Warning Message from sample library", status = DroidToast.WARNING)
     }
 }
--- droidtoast/src/main/java/code/sandeep/droidtoast/DroidToast.kt
+++ droidtoast/src/main/java/code/sandeep/droidtoast/DroidToast.kt
@@ -1,11 +1,80 @@
 package code.sandeep.droidtoast
 
+import android.app.Activity
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 
+/**
+ * @author Sandeep Karanam
+ * @date 30 Aug 2023
+ * This class is a static class which is responsible for showing the toast to the user based on the
+ * status of the request or the message which want to showcase.
+ *
+ */
 object DroidToast {
 
-    fun showToast(context: Context, message: String, duration: Int = Toast.LENGTH_LONG) {
-        Toast.makeText(context, message, duration).show()
+    /**
+     * These constants SUCCESS, FAILURE, and WARNING are used to set the color of the text based
+     * on the status of the message want to showcase to the users
+     */
+    const val SUCCESS = "success"
+    const val FAILURE = "failure"
+    const val WARNING = "warning"
+
+    /**
+     * This function is invoked by using ClassName itself.
+     * @param context The context can be activity or the application
+     * @param message The message which want to showcase on the toast
+     * @param duration The duration of the toast by default its Toast.LENGTH_LONG
+     * @param status The status of the toast which want to showcase to the users by default its success.
+     */
+    fun showToast(
+        context: Context,
+        message: String,
+        duration: Int? = Toast.LENGTH_LONG,
+        status: String? = SUCCESS
+    ) {
+        // Get the root view for the activity or fragment to access the element in the toast customized view
+        val view = LayoutInflater.from(context).inflate(
+            R.layout.alert, (context as Activity).window.decorView.rootView as ViewGroup, false
+        )
+        // Get the toast object
+        val toast = Toast.makeText(context, "", duration ?: Toast.LENGTH_LONG)
+        (view.findViewById(R.id.tv_message) as TextView).text = message
+        toast?.also {
+            when (status) {
+                SUCCESS -> {
+                    (view.findViewById(R.id.iv_message) as ImageView).setImageResource(R.drawable.icon_success)
+                    (view.findViewById(R.id.tv_message) as TextView).setTextColor(
+                        (context).resources.getColor(
+                            R.color.success_color, null
+                        )
+                    )
+                }
+                FAILURE -> {
+                    (view.findViewById(R.id.iv_message) as ImageView).setImageResource(R.drawable.icon_failure)
+                    (view.findViewById(R.id.tv_message) as TextView).setTextColor(
+                        (context).resources.getColor(
+                            R.color.failure_color, null
+                        )
+                    )
+                }
+                WARNING -> {
+                    (view.findViewById(R.id.iv_message) as ImageView).setImageResource(R.drawable.icon_warning)
+                    (view.findViewById(R.id.tv_message) as TextView).setTextColor(
+                        (context).resources.getColor(
+                            R.color.warning_color, null
+                        )
+                    )
+                }
+
+            }
+            it.view = view
+            it.show()
+        }
     }
 }
